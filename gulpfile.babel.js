9b28a3c55e116765b0ee68a9e10595ee3a2b16b4
Adrian Simmons <adrian@perlucida.com>
2015-12-18 19:40:37 +0000

    Working SASS processing.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,31 @@
 'use strict';
 
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import sourcemaps from 'gulp-sourcemaps';
 
 
-gulp.task('default', () => {
-  // do stuff
+const dirs = {
+  src: 'src',
+  dest: 'hugo/static'
+};
+
+
+const sassPaths = {
+  src: `${dirs.src}/sass/main.scss`,
+  dest: `${dirs.dest}/styles/`
+};
+
+
+gulp.task('styles', () => {
+  return gulp.src(sassPaths.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(sassPaths.dest));
 });
+
+
+//gulp.task('default', gulp.series(console.log('Default task called')));
