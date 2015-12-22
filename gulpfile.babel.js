8f6265ffb18b6a38743ba912e92c37fd9a34bad5
Adrian Simmons <adrian@perlucida.com>
2015-12-22 01:58:27 +0000

    Swap gulp-autoprefix for Postcss and autoprefix

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,8 +2,9 @@
 
 import gulp from 'gulp';
 import sass from 'gulp-sass';
-import autoprefixer from 'gulp-autoprefixer';
+import autoprefixer from 'autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
+import postcss from 'gulp-postcss';
 
 
 const dirs = {
@@ -19,13 +20,15 @@ const sassPaths = {
 
 
 gulp.task('styles', () => {
+  let processors = [autoprefixer];
   return gulp.src(sassPaths.src)
     .pipe(sourcemaps.init())
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(autoprefixer())
+    //.pipe(autoprefixer())
+    .pipe(postcss(processors))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(sassPaths.dest));
 });
 
 
-//gulp.task('default', gulp.series(console.log('Default task called')));
+gulp.task('default', gulp.series('styles'));
