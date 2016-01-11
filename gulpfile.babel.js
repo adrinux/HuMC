b6502aaa6c25fca20ac0055b7be26496a9e44aae
Adrian Simmons <adrian@perlucida.com>
2016-01-11 00:31:01 +0000

    Add comments and commented code from gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -60,11 +60,77 @@ gulp.task('scripts', () => {
     .pipe($.rename({extname: '.min.js'}))
     .pipe(gulp.dest(dirs.dest+'scripts/'));
 });
+// eslint on gulpfile? as well as scripts
+// eslint vs uglify
+// babel compilation of ES6? Still needed?
 
 
 // Image processing (with gm/im)
 
+// THIS NEEDS CONVERTED
+// Process Images with graphicsmagick or imagemagick
+// To use:
+// - uncomment section starting with gm below
+// - uncomment the task 'process' at the bottom of this file
+// - create folder for unprocessed images: "mkdir app/images_raw"
+// - install grunt-gm: "npm install grunt-gm --save-dev"
+// - run "grunt process" to process new images (add into a build or watch
+// task if you're doing this a lot)
+// You can configure subfolders etc if you need multiple processing steps
+// See https://github.com/h0ward/grunt-gm for Usage
+// The following tasks are just an example, fit to your needs.
+// gm: {
+//   crop: {
+//     options: {
+//       // uses graphicsmagick uness imagemagick is set as an option
+//       // default: false, check if dest file exists and size > 0
+//       skipExisting: true,
+//       // default: false
+//       stopOnError: false,
+//     },
+//     files: [{
+//       expand: true,
+//       cwd: '<%%= config.app %>/images_raw',
+//       src: ['**.{jpg,gif,png}'],
+//       dest: '<%%= config.app %>/images',
+//       filter: 'isFile',
+//       options: {
+//         // imageMagick: true,
+//         stopOnError: true
+//       },
+//       // image is passed as stream beteen tasks
+//       tasks: [
+//         { // scale
+//           resize: [500,250, "^"],
+//         },{ // crop
+//           gravity: ['Center'],
+//           extent: [250,125]
+//         }
+//       ]
+//     }],
+//   }
+// },
+
+// Responsive Images
+// Generate diiferent sized images for srcset
+
+
 // Image optimisation
+// Image optimisation task using imageoptim and jpegmini *OSX only*
+// If you disable this then tweak the imagemin task above to process jpg,png
+// imagemin task deals with gif, imageoptim with jpg and png
+// Switch jpegMini to false if its not installed. You may need to launch
+// JPEGmini.app before this task runs.
+// imageOptim: {
+//   optimise: {
+//     options: {
+//       jpegMini: true,
+//       imageAlpha: true,
+//       quitAfter: false
+//     },
+//     src: ['app/images/*.{jpg,jpeg,png}']
+//   }
+// },
 
 
 // Hugo
@@ -105,16 +171,20 @@ gulp.task('hugoLive', () => {
 });
 
 
+// Wiredep
+// modernizr, generate a custom build like generator-webapp
+// Susy?
+
 // HTML Linting
 // HTML minification?
 // Will need to run hugo first, then check output?
 
 
 // Testing
+// Jasmine, Mocha...
 
 
 // Cleaning
-
 // Specific cleaning tasks for dev/stage/live of hugo/published.
 gulp.task('clean:dev', () => {
   return Promise.all([ del('hugo/published/dev/') ])
@@ -130,11 +200,49 @@ gulp.task('clean:live', () => {
 // browsersync too
 
 // Build (staging and prod)?
-// Is this really needed
+// Is this really needed. Might simplify task definitions
 
 // serve/watch (dev)
 // needs to spawn a hugo watch task too (and pass back hugo errors)
 
+
+// Deploy
+// NEES CONVERSION
+// Update stage and production sites
+// deleteAll: Deletes objects in dest that aren't present in src or are
+// specifically excluded. Prevents orphan files server side.
+// Uncomment this section, configure dest/host.
+// Uncomment the upstage and upprod task at the bottom of this gruntfile
+// Make sure you can rsync from the CLI before running the 'grunt upstage'
+// or 'grunt upprod' tasks.
+// rsync: {
+//   options: {
+//     // -r recursive
+//     // -t preserve modification times
+//     // -o preserve owner
+//     // -v verbose
+//     // -z compress during transfer
+//     // --chmod=ugo=rwX use destination default permissions
+//     args: ['-rtozv', '--chmod=ugo=rwX', '--delete'],
+//     exclude: ['.git*', 'cache', 'logs', '.DS_Store'],
+//   },
+//   stage: {
+//     options: {
+//       src: 'dist/',
+//       dest: '/var/www/sitename-stage',
+//       host: 'you@stage.example.com',
+//       deleteAll: true
+//     },
+//   },
+//   production: {
+//     options: {
+//       src: 'dist/',
+//       dest: '/var/www/sitename',
+//       host: 'you@example.com',
+//       deleteAll: true
+//     },
+//   }
+// },
 
 // Tasks
 gulp.task('default', gulp.parallel('styles','scripts'));
