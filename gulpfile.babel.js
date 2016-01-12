6c280727c755eafcf9b54269ab3148a386b7bc27
Adrian Simmons <adrian@perlucida.com>
2016-01-12 17:29:19 +0000

    eslint and babel configuration added or expanded. syntax changes following that

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import 'babel-polyfill';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import del from 'del';
@@ -19,15 +20,14 @@ const dirs = {
 };
 
 const sassPaths = {
-  src: dirs.src+'sass/main.scss',
-  dest: dirs.dest+'styles/'
+  src: dirs.src + 'sass/main.scss',
+  dest: dirs.dest + 'styles/'
 };
 
 const hugoPaths = {
   StageBaseUrl: "http://stage.example.com/",
-  LiveBaseUrl: "http://example.com/",
+  LiveBaseUrl: "http://example.com/"
 };
-
 
 // CSS processing, Linting
 gulp.task('styles', () => {
@@ -46,24 +46,22 @@ gulp.task('styles', () => {
 
 // CSS minification and revision
 gulp.task('minstyles', () => {
-  return gulp.src(sassPaths.dest+'main.css')
+  return gulp.src(sassPaths.dest + 'main.css')
     .pipe($.minifyCss())
     .pipe($.rename({extname: '.min.css'}))
     .pipe(gulp.dest(sassPaths.dest));
 });
 
-
 // Javascript processing and minification
 gulp.task('scripts', () => {
-  return gulp.src(dirs.src+'scripts/*.js')
+  return gulp.src(dirs.src + 'scripts/*.js')
     .pipe($.uglify())
     .pipe($.rename({extname: '.min.js'}))
-    .pipe(gulp.dest(dirs.dest+'scripts/'));
+    .pipe(gulp.dest(dirs.dest + 'scripts/'));
 });
 // eslint on gulpfile? as well as scripts
 // eslint vs uglify
 // babel compilation of ES6? Still needed?
-
 
 // Image processing (with gm/im)
 
@@ -114,7 +112,6 @@ gulp.task('scripts', () => {
 // Responsive Images
 // Generate diiferent sized images for srcset
 
-
 // Image optimisation
 // Image optimisation task using imageoptim and jpegmini *OSX only*
 // If you disable this then tweak the imagemin task above to process jpg,png
@@ -132,44 +129,38 @@ gulp.task('scripts', () => {
 //   }
 // },
 
-
 // Hugo
 // -D is buildDrafts
 // -F is buildFuture
-function hugo(status) {
-
+function hugo (status) {
   let exec = require('child_process').execSync;
   let cmd = 'hugo --config=hugo/config.toml -s hugo/';
-  if (status == 'stage') {
-      cmd += ' -D -d published/stage/ --baseURL="' + hugoPaths.StageBaseUrl + '"';
-      $.gutil.log('hugo command: \n' + cmd);
-  }
-  else if (status == 'live') {
-      cmd += ' -d published/live/ --baseURL="' + hugoPaths.LiveBaseUrl + '"';
-      $.gutil.log('hugo command: \n' + cmd);
-  }
-  else {
-      cmd += ' -DF -d published/dev/';
-      $.gutil.log('hugo command: \n' + cmd);
+  if (status === 'stage') {
+    cmd += ' -D -d published/stage/ --baseURL="' + hugoPaths.StageBaseUrl + '"';
+    $.gutil.log('hugo command: \n' + cmd);
+  } else if (status === 'live') {
+    cmd += ' -d published/live/ --baseURL="' + hugoPaths.LiveBaseUrl + '"';
+    $.gutil.log('hugo command: \n' + cmd);
+  } else {
+    cmd += ' -DF -d published/dev/';
+    $.gutil.log('hugo command: \n' + cmd);
   }
 
   let result = exec(cmd, {encoding: 'utf-8'});
   $.gutil.log('hugo reports: \n' + result);
-
 }
 
 gulp.task('hugoDev', () => {
-  return Promise.all([ hugo() ])
+  return Promise.all([ hugo() ]);
 });
 
-gulp.task('hugoStage',  () => {
-  return Promise.all([ hugo('stage') ])
+gulp.task('hugoStage', () => {
+  return Promise.all([ hugo('stage') ]);
 });
 
 gulp.task('hugoLive', () => {
-  return Promise.all([ hugo('live') ])
+  return Promise.all([ hugo('live') ]);
 });
-
 
 // Wiredep
 // modernizr, generate a custom build like generator-webapp
@@ -179,21 +170,19 @@ gulp.task('hugoLive', () => {
 // HTML minification?
 // Will need to run hugo first, then check output?
 
-
 // Testing
 // Jasmine, Mocha...
-
 
 // Cleaning
 // Specific cleaning tasks for dev/stage/live of hugo/published.
 gulp.task('clean:dev', () => {
-  return Promise.all([ del('hugo/published/dev/') ])
+  return Promise.all([ del('hugo/published/dev/') ]);
 });
 gulp.task('clean:stage', () => {
-  return Promise.all([ del('hugo/published/stage/') ])
+  return Promise.all([ del('hugo/published/stage/') ]);
 });
 gulp.task('clean:live', () => {
-  return Promise.all([ del('hugo/published/live/') ])
+  return Promise.all([ del('hugo/published/live/') ]);
 });
 
 // Watch for changes
@@ -204,7 +193,6 @@ gulp.task('clean:live', () => {
 
 // serve/watch (dev)
 // needs to spawn a hugo watch task too (and pass back hugo errors)
-
 
 // Deploy
 // NEES CONVERSION
@@ -245,8 +233,8 @@ gulp.task('clean:live', () => {
 // },
 
 // Tasks
-gulp.task('default', gulp.parallel('styles','scripts'));
-gulp.task('dev', gulp.series('clean:dev','styles','scripts','hugoDev'));
-gulp.task('stage', gulp.series('clean:stage', gulp.parallel('styles','scripts'),'minstyles','hugoStage'));
-gulp.task('live', gulp.series('clean:live', gulp.parallel('styles','scripts'),'minstyles','hugoLive'));
+gulp.task('default', gulp.parallel('styles', 'scripts'));
+gulp.task('dev', gulp.series('clean:dev', 'styles', 'scripts', 'hugoDev'));
+gulp.task('stage', gulp.series('clean:stage', gulp.parallel('styles', 'scripts'), 'minstyles', 'hugoStage'));
+gulp.task('live', gulp.series('clean:live', gulp.parallel('styles', 'scripts'), 'minstyles', 'hugoLive'));
 
