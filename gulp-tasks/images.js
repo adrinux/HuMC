8f3ccb273fabd85f12fc0dd16d853d83c92021e9
Adrian Simmons <adrian@perlucida.com>
2016-01-28 10:36:33 +0000

    Add test image and code to call gulp-sharp

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,12 +1,23 @@
 //
 // Process images
-module.exports = function (gulp, plugins) {
-  return function () {
-    gulp.src('src/img_raw/')
-    .pipe(plugins.)
-    .pipe(gulp.dest('src/img_processed/'));
-  };
+// before use install vips then gulp-sharp
+// for vips see: https://www.npmjs.com/package/gulp-sharp
+// then: npm install gulp-sharp --save-dev
+
+// Move this to external config
+let sharpOptions = {
+  resize : [1280, 800],
+  max : true,
+  withoutEnlargement: true,
+  quality : 80,
+  progressive : false
 };
 
 
-// Decide which software to use...graphicsmagick or sharp
+module.exports = function (gulp, plugins) {
+  return function () {
+    gulp.src('src/img_raw/')
+    .pipe(plugins.sharp(sharpOptions))
+    .pipe(gulp.dest('src/img_processed/'));
+  };
+};
