644c89ccd8de684d5152116a53971aa50df242c4
Adrian Simmons <adrian@perlucida.com>
2019-04-15 16:13:40 +0100

    Script clean up

--- updates/deprecated-RSSLink.sh
+++ updates/deprecated-RSSLink.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
+### .RSSLink is deprecated
+# For an error/warning from Hugo like:
+# "Page's .RSSLink is deprecated and will be removed in a future release. Use the Output Format's link, e.g. something like: 
+#   {{ with .OutputFormats.Get "RSS" }}{{ .RelPermalink }}{{ end }}."
 
-#find_text='{{ if .RSSLink }}
+
+# find old style code:
+# {{ if .RSSLink }}
 # <link href="{{ .RSSLink }}" rel="alternate" type="application/rss+xml" title="{{ .Site.Title }}" />
 # <link href="{{ .RSSLink }}" rel="feed" type="application/rss+xml" title="{{ .Site.Title }}" />
-# {{ end }}'
+# {{ end }}
 
-# replacement_text='{{ with .OutputFormats.Get "rss" -}}
+# Replace with new style:
+# {{ with .OutputFormats.Get "rss" -}}
 # {{ printf `<link rel="%s" type="%s" href="%s" title="%s" />` .Rel .MediaType.Type .Permalink $.Site.Title | safeHTML }}
-# {{ end -}}'
+# {{ end -}}
 
 
 themes_with_rsslink=$(grep -lr "if .RSSLink")
@@ -26,26 +33,3 @@ do
   echo "---}"
 )
 done
-
-
-
-
-
-### .RSSLink is deprecated
-# "Page's .RSSLink is deprecated and will be removed in a future release. Use the Output Format's link, e.g. something like: 
-#   {{ with .OutputFormats.Get "RSS" }}{{ .RelPermalink }}{{ end }}."
-
-# gawk ‘/start/{f=1;print;while (getline < “deprecated-RSSLink.txt”){print}}/end/{f=0}!f’ filetochange
-
-#grep --null -lr "if .RSSLink" | xargs --null sed -i '/{{ if .RSSLink }}/,/{{ end }}/' 's/old_string/new_string/g'
-
-#grep --line-buffered --null -lr "if .RSSLink" | xargs --null gawk ‘/\{\{ if .RSSLink \}\}/{f=1;print;while (getline < “/home/adrian/Admin/HuMC/updates/deprecated-RSSLink.txt”){print}}/\{\{ end \}\}/{f=0}!f’
-
-#grep --line-buffered -lr "if .RSSLink" | xargs gawk ‘/\{\{ if .RSSLink \}\}/{f=1;print;while (getline < “/home/adrian/Admin/HuMC/updates/deprecated-RSSLink.txt”){print}}/\{\{ end \}\}/{f=0}!f’
-
-
-
-
-
-
-
